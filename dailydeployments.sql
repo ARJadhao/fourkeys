592e3b2dcdb477d83ae350adffa6387ea9da9173
ARJadhao <ashishjadhao17@gmail.com>
2021-10-01 08:47:25 +0530

    Revert "additional views for dashboards"
    This reverts commit cb6c878c7317edc277d834a3a092cb8ae3314f35.

--- dailydeployments.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-TIMESTAMP_TRUNC(time_created, DAY) AS day,
-COUNT(distinct deploy_id) AS deployments
-FROM
-four_keys.deployments
-GROUP BY day;
--- dailymediantimetorestore.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  TIMESTAMP_TRUNC(time_created, DAY) as day,
-  #### Median time to resolve
-  PERCENTILE_CONT(
-    TIMESTAMP_DIFF(time_resolved, time_created, HOUR), 0.5)
-    OVER(PARTITION BY TIMESTAMP_TRUNC(time_created, DAY)
-    ) as daily_med_time_to_restore,
-  FROM four_keys.incidents;
